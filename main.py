23ba7100c7fe836a09cf211f0360b7316925c321
Tanzim-Tousif <2105071@ugrad.cse.buet.ac.bd>
2025-11-30 19:47:47 +0600

    m

--- main.py
+++ main.py
@@ -75,4 +75,4 @@ def add_user(user: UserCreate, db: Session = Depends(get_db)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
